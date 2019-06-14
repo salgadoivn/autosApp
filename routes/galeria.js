3ceb809c8647631fb1fde2457343f86e7569593f
salgadoivn <sagmivan@gmail.com>
2019-06-14 11:57:48 -0500

    se agrega la ruta /mazda /toyota /galeria/mazda

---
+++ routes/galeria.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/mazda', function (req, res, next){
+	
+	var data={autos:[]};
+	var auto={};
+	auto.nombre="Mazda 3";
+	auto.Foto="https://www.actualidadmotor.com/wp-content/uploads/2019/01/Mazda-3-1250x515.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+	auto.nombre="Mazda 6";
+	auto.Foto="https://www.mazda.mx/siteassets/mazda-mx/mycos-2020/mazda6/vlp/360/gris-titanio/mazda-6-vehiculos-exterior-gris-titanio-17.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+	auto.nombre="Mazda CX-3";
+	auto.Foto="https://www.mazdausa.com/siteassets/vehicles/2019/cx-3/vlp/studio-360s/machine-gray/my19-cx3-gt-machine-gray-0002.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+	auto.nombre="Mazda 6";
+	auto.Foto="https://www.mazda.mx/siteassets/mazda-mx/mycos-2020/mazda6/vlp/360/gris-titanio/mazda-6-vehiculos-exterior-gris-titanio-17.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+	auto.nombre="Mazda CX-3";
+	auto.Foto="https://www.mazdausa.com/siteassets/vehicles/2019/cx-3/vlp/studio-360s/machine-gray/my19-cx3-gt-machine-gray-0002.jpg";
+	data.autos.push(auto);
+	
+	auto=null;
+	auto={};
+	auto.nombre="Mazda 6";
+	auto.Foto="https://www.mazda.mx/siteassets/mazda-mx/mycos-2020/mazda6/vlp/360/gris-titanio/mazda-6-vehiculos-exterior-gris-titanio-17.jpg";
+	data.autos.push(auto);
+
+	auto=null;
+	auto={};
+	auto.nombre="Mazda CX-3";
+	auto.Foto="https://www.mazdausa.com/siteassets/vehicles/2019/cx-3/vlp/studio-360s/machine-gray/my19-cx3-gt-machine-gray-0002.jpg";
+	data.autos.push(auto);
+
+
+	console.log(data)
+
+	res.render("./galeria/mazda", data);
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'autosApp' });
 });
+
+router.get('/Mazda', function (req, res, next){
+	var infoMazda={};
+	infoMazda.pais='Japón';
+	infoMazda.sectorVentas='Comercial';
+	infoMazda.anioFundacion=1929;
+	infoMazda.logo="https://www.mazda.mx/siteassets/mazda-mx/logos-new-mazda/mazda-logo-desktop-2.png";
+
+	res.render('Mazda', infoMazda);
+});
+
+router.get('/Toyota', function (req, res, next){
+	var infoToyo={};
+	infoToyo.pais='Japón';
+	infoToyo.sectorVentas='Comercial';
+	infoToyo.anioFundacion=1937;
+	infoToyo.logo="http://www.car-brand-names.com/wp-content/uploads/2015/07/Toyota-logo.png";
+
+	res.render('Toyota', infoToyo);
+});	
 
 module.exports = router;
